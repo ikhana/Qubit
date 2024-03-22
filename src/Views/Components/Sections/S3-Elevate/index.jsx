b5f594a45d5696a40637c1cee450c2a101b48b27
ikhana <Inaa.eth@gmail.com>
2024-03-23 04:12:23 +0500

    paddings and margins

--- src/Views/Components/Sections/S3-Elevate/index.jsx
+++ src/Views/Components/Sections/S3-Elevate/index.jsx
@@ -27,9 +27,9 @@ const ElevateExperience = () => {
       <MotionCont maxWidth="lg" initial={{y:"20%"}} whileInView={{y:"0%"}} viewport={{once: true}} transition={{duration: 1}} sx={{display:"flex", flexDirection:"column", justifyContent:"center",alignItems:"center", paddingBottom:"100px",paddingTop:{xs:"0px", md:"50px"} }}>
         <Maintxt textAlign="center" my={2} color={nvidiaGreen}>Visionary QubitN</Maintxt>
         <Paragraph color="#fff" sx={{paddingBottom:"20px"}} maxWidth="900px" textAlign="center">Dive into the world of QubitN where future-proof technologies and community-driven innovation forge the path to a decentralized future.</Paragraph>
-        <Grid container gap={1} alignItems="start" justifyContent="center">
-        <Cardimg xs={12} sm={12} md={3.5} padd="0 26%" psrc={Pic2} bg="transparent" def1="Community-Driven Innovation" def2="At QubitN, innovation is not a solo journey. It’s a collaborative effort that thrives on the diverse ideas and contributions of our global community. Join us to shape the future of decentralized computing, where every voice matters and every idea can spark change." />
-          <Cardimg xs={12} sm={12} md={4} padd="0 26%" psrc={Pic1} bg="transparent" def1="Future-Proof Technologies" def2="Embark on a journey with QubitN as we explore and integrate cutting-edge technologies like quantum computing and next-gen encryption to ensure your digital infrastructure is not just for today, but tomorrow and beyond." />
+        <Grid container gap={20} alignItems="start" justifyContent="center">
+        <Cardimg xs={12} sm={12} md={4.5} padd="5% 26%" psrc={Pic2} bg="transparent" def1="Community-Driven Innovation" def2="At QubitN, innovation is not a solo journey. It’s a collaborative effort that thrives on the diverse ideas and contributions of our global community. Join us to shape the future of decentralized computing, where every voice matters and every idea can spark change." />
+          <Cardimg xs={12} sm={12} md={4} padd="7% 26%" psrc={Pic1} bg="transparent" def1="Future-Proof Technologies" def2="Embark on a journey with QubitN as we explore and integrate cutting-edge technologies like quantum computing and next-gen encryption to ensure your digital infrastructure is not just for today, but tomorrow and beyond." />
 
         </Grid>
       </MotionCont>
